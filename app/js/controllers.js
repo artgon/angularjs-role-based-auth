9a1e0daf3092a78cd379012de2421e31a946c318
Arthur Gonigberg <artgon@gmail.com>
2013-10-20 18:17:28 -0700

    added comments

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -3,10 +3,12 @@ app.controller('LoginController', function ($scope, AuthenticationService) {
     'use strict';
 
     $scope.loginUser = function() {
+      // this should be replaced with a call to your API for user verification (or you could also do it in the service)
       AuthenticationService.login({name: 'User', role: 'user'});
     };
 
     $scope.loginAdmin = function() {
+      // this should be replaced with a call to your API for user verification (or you could also do it in the service)    
       AuthenticationService.login({name: 'Admin', role: 'admin'});
     };
 });
